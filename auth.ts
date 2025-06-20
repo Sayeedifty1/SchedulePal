1146c5e5d6e57d65ffcf5c4d3eaab50b60995e24
Sayeedifty1 <md.abusayeedifty@gmail.com>
2025-06-20 23:53:11 +0200

    auth js, prisma installed

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import NextAuth from "next-auth";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+});
