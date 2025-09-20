71f28f33c31251b8efaea16b08a18b089223a803
Sayeedifty1 <md.abusayeedifty@gmail.com>
2025-09-20 23:52:03 +0200

    auth session model and auth helper initiated

--- auth.ts
+++ auth.ts
@@ -1,9 +1,76 @@
 import { prisma } from "@/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
 
 import NextAuth from "next-auth";
+import { getGoogleAccountByEmail } from "@/features/auth/user-auth-session-model.server";
+import {
+  isAssessTokenExpired,
+  refreshAndUpdateAccessToken,
+  setSessionAccessToken,
+} from "@/features/auth/authentication-helper.server";
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    GoogleProvider({
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/calender.events https://www.googleapis.com/auth/gmail.send",
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn() {
+      return true;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expires_at = account.expires_at;
+
+        // save these tokens in the database (to keep them persistent)
+        await prisma.account.updateMany({
+          where: { providerAccountId: account.providerAccountId },
+          data: {
+            access_token: account.access_token,
+            refresh_token: account.refresh_token,
+            expires_at: account.expires_at,
+          },
+        });
+      }
+      return token;
+    },
+    async session({ session }) {
+      const account = await getGoogleAccountByEmail(session.user.email ?? "");
+      if (!account) return session;
+
+      // check if the token is expired
+      const accessToken = isAssessTokenExpired(account.expires_at)
+        ? await refreshAndUpdateAccessToken(account) //refresh if expired
+        : account.access_token; // else use the existing one
+
+      //inject fresh access token in the session
+      return setSessionAccessToken(session, accessToken);
+    },
+  },
+
+  secret: process.env.AUTH_SECRET,
+
+  debug: process.env.NODE_ENV === "development",
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,22 @@
-export { auth as middleware } from "../auth";
+import { NextRequest, NextResponse } from "next/server";
+//next-env.d.ts
+declare module "next/server" {
+  interface NextRequest {
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    auth: any;
+  }
+}
+export default function middleware(request: NextRequest) {
+  const isAuthenticated = !!request.auth;
+  const dashboard = request.nextUrl.pathname.startsWith("/dashboard");
+  if (dashboard && !isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
+  }
+  return NextResponse.next();
+}
+
+//Run middleware on the dashboard route
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
+//this middleware checks if the user is authenticated before allowing access to the dashboard route. If not authenticated, it redirects to the login page.
